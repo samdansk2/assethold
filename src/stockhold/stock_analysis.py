8cff379a82598b5f9bd190707dca2d4d24a168ba
Vamsee Achanta <vamsee.achanta@aceengineer.com>
2022-11-01 20:33:39 -0500

    analysis working

--- src/stockhold/stock_analysis.py
+++ src/stockhold/stock_analysis.py
@@ -1,11 +1,35 @@
+from stockhold.common.data import AttributeDict
 from stockhold.finance_components import FinanceComponents
 
 
-def stock_analysis(cfg):
+def stock_analysis(ticker, dashboard_flag=True):
+    cfg = get_detailed_cfg(ticker, dashboard_flag)
     fc = FinanceComponents(cfg)
+
     fc.fdata.get_data()
     stock_data_dict = fc.get_data_dict()
-
     fc.perform_analysis(stock_data_dict)
+
     if cfg['dashboard']:
         fc.dashboard()
+
+
+def get_detailed_cfg(ticker, dashboard_flag):
+    cfg = AttributeDict({
+        "stocks": [{
+            "ticker": ticker
+        }],
+        "dashboard": dashboard_flag,
+        "source": "yfinance"
+    })
+
+    cfg_sec = {
+        'filing_type': '4',
+        'num_filings_to_download': 10,
+        'include_amends': False,
+        'after_date': None,
+        'before_date': None
+    }
+    cfg.update({"cfg_sec": cfg_sec})
+
+    return cfg
--- src/stockhold/test/test_all_analysis_xom.py
+++ src/stockhold/test/test_all_analysis_xom.py
@@ -1,20 +1,6 @@
 from stockhold import stock_analysis
-from stockhold.common.data import AttributeDict
 
-cfg_sec = {
-    'filing_type': '4',
-    'num_filings_to_download': 10,
-    'include_amends': False,
-    'after_date': None,
-    'before_date': None
-}
-cfg = AttributeDict({
-    "stocks": [{
-        "ticker": "XOM"
-    }],
-    "source": "yfinance",
-    "cfg_sec": cfg_sec,
-    "dashboard": True
-})
+ticker = "XOM"
+dashboard_flag = True
 
-stock_analysis.stock_analysis(cfg)
+stock_analysis.stock_analysis(ticker, dashboard_flag)
