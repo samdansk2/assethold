ca876dd7d09fd6d8525a8465c5361b23e4c2c09a
Vamsee Achanta <achantav@gmail.com>
2024-10-09 11:07:05 -0500

    wip

---
+++ src/assethold/analysis/portfolio.py
@@ -0,0 +1,20 @@
+
+class Portfolio():
+
+    def __init__(self, assets):
+        pass
+        
+    def TBA(self, asset):
+        pass
+    
+    '''
+    cash in or cash out, dividend payout. 
+    value of portfolio. dercerase in asset value
+    
+    if you are depositing cash in to the account, then this increases. 1 month 1000
+    if you are withdrawing cash from the account, then this decreases. 1 month -1000
+    
+    if a stock pays dividend, add dividend to the cash value.
+    if a stock is sold, then add the value of the stock to the cash value.
+    
+    '''
