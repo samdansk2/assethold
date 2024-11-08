feaf5363789808e199cf3e1eb730e11a92ce61c0
Vamsee Achanta <achantav@gmail.com>
2024-11-08 11:14:00 -0600

    20240811

--- src/assethold/tests/test_data/analysis/Portfolio/calculate_profit.py
+++ src/assethold/tests/test_data/analysis/Portfolio/calculate_profit.py
@@ -1,19 +1,32 @@
+# Third party imports
 import yfinance as yf
 
 ticker = 'XOM'  
-initial_investment = 100  
+initial_investment = 100
+investment_frequency = 'single'
+investment_frequency = '1mo' # monthly
+buy_date = '2020-01-01'
 start_date = '2020-01-01'
-end_date = '2024-01-01'
+end_date = '2024-11-01'
+interval = '1mo'
 
-data = yf.download(ticker, start=start_date, end=end_date, interval='1mo')
+stock_ticker_object = yf.Ticker(ticker)
+dividends = stock_ticker_object.dividends
+
+price_data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
 
 # monthly profit
-data['Price Change %'] = data['Adj Close'].pct_change() * 100 # percentage change
-data['Monthly Profit'] = data['Price Change %'] * (initial_investment / 100)   
+# data['Price Change %'] = data['Adj Close'].pct_change() * 100 # percentage change
+# data['Monthly Profit'] = data['Price Change %'] * (initial_investment / 100)
 
-df = data[['Adj Close', 'Price Change %', 'Monthly Profit']].dropna().reset_index()
-df.columns = ['Date', 'Adj Close', 'Price Change %', 'Monthly Profit']
+price_data['Price Change %'] = price_data['Adj Close'].iloc[:].subtract(price_data['Adj Close'].iloc[0])
 
-print(df)
+price_data['overall_profit'] = price_data['Price Change %'] * (initial_investment / 100)
+price_data['value'] = price_data['overall_profit'].add(initial_investment)
+
+# df = price_data[['Adj Close', 'Price Change %', 'overall_profit']].dropna().reset_index()
+# df.columns = ['Date', 'Adj Close', 'Price Change %', 'overall_profit']
+
+print(price_data)
 csv_path = r'src\assethold\tests\test_data\analysis\Portfolio\results\Data'
-df.to_csv(f'{csv_path}\\monthly_profit_{ticker}.csv', index=False)
+price_data.to_csv(f'{csv_path}\\overall_profit_{ticker}.csv', index=False)
