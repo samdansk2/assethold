ee8fd36a48ec7fb455e700a0d8c3086f286df972
samdansk2 <samdanshaik8998@gmail.com>
2024-11-29 12:54:48 +0530

    wip

--- src/assethold/modules/stocks/investment_value_fnn.py
+++ src/assethold/modules/stocks/investment_value_fnn.py
@@ -13,25 +13,10 @@ class InvestmentValueFnn:
         self.get_returns(cfg, prices_data)
 
         return cfg, prices_data
-
-    def get_returns(self, cfg, prices_data):
-        """
-        Process historical price data to calculate returns.
-        """
-        prices_data = prices_data.copy()
-        returns = ffn.to_returns(prices_data)
-        
-        ax = returns.hist(figsize=(12, 5))
-        # plt.show()
-        
-        ax = prices_data.rebase().plot(figsize=(12,5))
-        #plt.show()
-
-        perf = prices_data.calc_stats()
         
     def prepare_data(self, data: pd.DataFrame) -> pd.DataFrame:
         """
-        Prepare data for analysis by ensuring the index is a DatetimeIndex.
+        fix up the data for analysis by ensuring the index is a DatetimeIndex.
         """
         if not isinstance(data.index, pd.DatetimeIndex):
             if 'Date' in data.columns:
@@ -42,6 +27,20 @@ class InvestmentValueFnn:
                 raise ValueError("Data must include a 'Date' column to set as a DatetimeIndex.")
         
         return data
+
+    def get_returns(self, cfg, prices_data):
+        """
+        Process historical price data to calculate returns.
+        """
+        prices_data = prices_data.copy()
+        prices_data['daily_returns'] = ffn.to_returns(prices_data['Close'])
+
+        #TODO - Add more return calculations
+        # - monthly_returns 
+        # - visualize the data
+
+
         
+        return prices_data
         
         
